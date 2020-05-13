f5c350b6b665a1f1813fbcc583f6771c8af349f4
nonhina <hinano.miyajima@sencorp.co.jp>
2020-05-13 15:24:28 +0900

    下の方に移動

--- yeahcheese/app/User.php
+++ yeahcheese/app/User.php
@@ -8,10 +8,6 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    public function events()
-    {
-        return $this->hasMany('App\Event');
-    }
     use Notifiable;
 
     /**
@@ -40,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function events()
+    {
+        return $this->hasMany('App\Event');
+    }
 }
