84682a8cd0eede373eb4981c75e2cf09a4e6763d
minami <m.kaori25@gmail.com>
2020-05-13 14:30:11 +0900

    イベントを表示

--- yeahcheese/app/Http/Controllers/EventController.php
+++ yeahcheese/app/Http/Controllers/EventController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function show(Request $request) //resquestに認証キー
+    {
+        $event = \App\Event::where('auth_key', $request)->get();
+        $picture = \App\Picture::where('event_id', $event->id)->get();
+        $data = [['pictures' => $picture], ['event' => $event]];
+        return view('event', $data);
+    }
 }
