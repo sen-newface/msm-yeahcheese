415822b063c696cc8ca10181f6e7e72c3af51c01
nonhina <hinano.miyajima@sencorp.co.jp>
2020-05-13 14:31:21 +0900

    イベントのデータを取ってくる

--- yeahcheese/app/Http/Controllers/EventController.php
+++ yeahcheese/app/Http/Controllers/EventController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Event;
 class EventController extends Controller
 {
-    //
+    public function index(){
+        $events = Event::with('pictures')->get();
+        return view('events_list', ['events' => $events]);
+    }
 }
