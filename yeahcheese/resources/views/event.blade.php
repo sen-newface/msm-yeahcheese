4ed52a851909048fc04785fbd6520debb86f66f7
minami <m.kaori25@gmail.com>
2020-05-14 10:48:47 +0900

    event.blade.phpの修正

--- yeahcheese/resources/views/event.blade.php
+++ yeahcheese/resources/views/event.blade.php
@@ -3,14 +3,13 @@
 @section('title', 'イベント')
 
 @section('content')
+    <div>
     @if(isset($event))
         <h2>{{ $event->title }}</h2><!-- eventのタイトル -->
     @else
         <h2>error</h2>
     @endif
+    <!-- ここに写真が入ります -->
     <!-- events.id == pictures.event_idのpictures.path -->
-    <div>
-    
-    <p>しゃしん</p>
     </div>
 @endsection
