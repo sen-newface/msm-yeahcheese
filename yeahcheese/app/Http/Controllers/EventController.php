2e4de022aa214b29fcc81087ac269a8938d6afbe
minami <m.kaori25@gmail.com>
2020-05-14 09:00:38 +0900

    showが2つになっていたのをひとつ消しました

--- yeahcheese/app/Http/Controllers/EventController.php
+++ yeahcheese/app/Http/Controllers/EventController.php
@@ -9,19 +9,6 @@ use \App\Picture;
 
 class EventController extends Controller
 {
-    public function show(Request $request) //resquestに認証キー
-    {
-        $event = \App\Event::where('auth_key', $request)->get();
-        $picture = \App\Picture::where('event_id', $event->id)->get();
-        $data = [['pictures' => $picture], ['event' => $event]];
-        return view('event', $data);
-    }
-
-    public function search()
-    {
-        return view('search');
-    }
-  
     public function index()
     {
         $events = Event::with('pictures')->get();
@@ -75,5 +62,10 @@ class EventController extends Controller
         $picture = Picture::where('event_id', $event->id)->get();
         $data = [['pictures' => $picture], ['event' => $event]];
         return view('event', $data);
+    }
+
+    public function search()
+    {
+        return view('search');
     }
 }
