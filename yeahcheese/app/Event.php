eb3b2c418e493e2f9eeaa545d8855c0720d930c8
kei-s16 <kenta.sasaki@sencorp.co.jp>
2020-05-15 10:44:29 +0900

    Eventモデルに公開終了日が指定の日付より前のものを取得するスコープを追加

--- yeahcheese/app/Event.php
+++ yeahcheese/app/Event.php
@@ -26,6 +26,11 @@ class Event extends Model
     
     }
 
+    public function scopeEndDateBefore($query, $date)
+    {
+    
+    }
+
     public static $rules = [
         'title' => ['required', 'max:255'],
         'release_date' => ['required', 'date', 'after_or_equal:today'],
