079df221a5858c53cc3618372660268bf92f9783
nonhina <hinano.miyajima@sencorp.co.jp>
2020-05-15 09:48:45 +0900

    APIのPictureControllerにfetchメソッドを追加

--- yeahcheese/app/Http/Controllers/Api/PictureController.php
+++ yeahcheese/app/Http/Controllers/Api/PictureController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Picture;
 
 class PictureController extends Controller
 {
-    //
+    public function fetch(Picture $picture)
+    {
+        # code...
+    }
 }
