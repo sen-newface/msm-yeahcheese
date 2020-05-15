37d06810d29ee91a0b3e9dd4751339941b7878e0
kei-s16 <kenta.sasaki@sencorp.co.jp>
2020-05-15 09:20:00 +0900

    Pictureを保存するAPIのルーティング情報を追記

--- yeahcheese/routes/api.php
+++ yeahcheese/routes/api.php
@@ -25,4 +25,5 @@ Route::prefix('events')->group(function () {
 
 Route::prefix('pictures')->group(function () {
     Route::get('/', 'Api\PictureController@fetch')->name('pictures.fetch');
+    Route::post('/', 'Api\PictureController@store')->name('pictures.store');
 });
