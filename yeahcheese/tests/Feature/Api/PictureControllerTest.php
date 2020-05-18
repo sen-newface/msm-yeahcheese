6c0ad022f8dc317ee962f9081b777916922bb7c1
minami <m.kaori25@gmail.com>
2020-05-18 12:06:02 +0900

    使っていないuseを削除

--- yeahcheese/tests/Feature/Api/PictureControllerTest.php
+++ yeahcheese/tests/Feature/Api/PictureControllerTest.php
@@ -1,9 +1,6 @@
 <?php
 
-namespace Tests\Feature\Api;
-
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PictureControllerTest extends TestCase
