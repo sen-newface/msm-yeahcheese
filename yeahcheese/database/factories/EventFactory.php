43caf784457074ba7eceea5c6a5d0eee90d2f404
kei-s16 <kenta.sasaki@sencorp.co.jp>
2020-05-18 16:23:20 +0900

    EventFactoryの設定を記述

--- yeahcheese/database/factories/EventFactory.php
+++ yeahcheese/database/factories/EventFactory.php
@@ -2,11 +2,16 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Event;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Event::class, function (Faker $faker) {
+    $datetime = $faker->dateTimeThisYear;
+
     return [
-        //
+        'title' => $faker->word,
+        'release_date' => $datetime->format('Y-m-d H:i'),
+        'end_date' => $datetime->modify('+7day')->format('Y-m-d H:i'),
+        'user_id' => factory(App\User::class),
     ];
 });
