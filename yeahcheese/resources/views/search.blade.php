70aa5481a26179d145a8c7e570d9e9c38a23ba71
kei-s16 <kenta.sasaki@sencorp.co.jp>
2020-05-19 12:15:40 +0900

    テンプレート内にエラー表示の処理を記述

--- yeahcheese/resources/views/search.blade.php
+++ yeahcheese/resources/views/search.blade.php
@@ -6,6 +6,11 @@
     <h2>イベント閲覧</h2>
     <h1>認証キー入力</h1>
     <div>
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+        <p>{{ $error }}</p>
+        @endforeach
+    @endif
         <form method="GET" action="{{ route('events.show') }}">
             <input type="text" name="auth_key" placeholder="認証キーを入力">
             <button type="submit">閲覧</button>
