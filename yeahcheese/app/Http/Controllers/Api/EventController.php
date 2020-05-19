44c0dd306c61a01b2ba32cf4ae3377e7b0c7c386
kei-s16 <kenta.sasaki@sencorp.co.jp>
2020-05-19 10:21:06 +0900

    Update関数の処理を記述

--- yeahcheese/app/Http/Controllers/Api/EventController.php
+++ yeahcheese/app/Http/Controllers/Api/EventController.php
@@ -17,8 +17,12 @@ class EventController extends Controller
         return new EventResources($event);
     }
 
-    public function update(Event $event)
+    public function update(Request $request)
     {
-        #
+        $event = Event::find($request->id);
+
+        if (!is_null($event)) {
+            $event->fill($request->all())->save();
+        }
     }
 }
