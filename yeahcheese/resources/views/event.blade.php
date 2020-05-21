8f44211a14eb5b99681b5d94a1edd88106c31574
minami <m.kaori25@gmail.com>
2020-05-21 17:49:54 +0900

    タイトルと画像がいい感じに見えるように変更

--- yeahcheese/resources/views/event.blade.php
+++ yeahcheese/resources/views/event.blade.php
@@ -4,16 +4,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col text-center"><h2>{{ $event->title }}</h2></div><!-- eventのタイトル -->
+    <div class="jumbotron">
+        <h2 class="">{{ $event->title }}</h2>
     </div>
+
+    <div class="container-fluid">
     <div class="row">
         <!-- 写真一覧表示 -->
         @foreach($pictures as $picture)
-        <div class="col text-center">
-            <img src="{{ \Storage::url($picture->path) }}" style="width:250px">
+        <div class="col-4 my-2">
+            <img src="{{ \Storage::url($picture->path) }}" width="100%" class="img img-thumbnail">
         </div>
         @endforeach
+    </div>
     </div>
 </div>
 @endsection
