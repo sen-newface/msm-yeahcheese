695c0a5fe4263c399aa7e56e1eec336a22d11686
minami <m.kaori25@gmail.com>
2020-05-21 11:57:24 +0900

    bootstrapを適用

--- yeahcheese/resources/views/event.blade.php
+++ yeahcheese/resources/views/event.blade.php
@@ -3,13 +3,17 @@
 @section('title', 'イベント')
 
 @section('content')
-    <div>
-        <h2>{{ $event->title }}</h2><!-- eventのタイトル -->
+<div class="container">
+    <div class="row">
+        <div class="col text-center"><h2>{{ $event->title }}</h2></div><!-- eventのタイトル -->
+    </div>
+    <div class="row">
         <!-- 写真一覧表示 -->
         @foreach($pictures as $picture)
-        <div>
+        <div class="col text-center">
             <img src="{{ \Storage::url($picture->path) }}" style="width:250px">
         </div>
         @endforeach
     </div>
+</div>
 @endsection
--- yeahcheese/resources/views/events_list.blade.php
+++ yeahcheese/resources/views/events_list.blade.php
@@ -3,23 +3,34 @@
 @section('title', 'イベント一覧')
 
 @section('content')
-    <h2>イベント一覧</h2>
-    <a href="{{ route('events.create') }}">
-        <button>新規作成</button>
+<div class="container">
+    <div class="row">
+        <h2 class="col-md-6 offset-md-3 text-center">イベント一覧</h2>
+    </div>
+    <div class="row">
+    <a class="col-md-2 offset-md-5" href="{{ route('events.create') }}">
+        <button type="button" class="btn btn-primary btn-lg btn-block">新規作成</button>
     </a>
+    </div>
     <!-- $eventsはで現在のユーザが作成したイベントの配列が渡される想定です -->
+    <div class="row">
     @foreach($events as $event)
-        <div>
-            <h3>{{ $event->title }}</h3>
-            <p>掲載期間：{{ $event->release_date }} - {{ $event->end_date }} 枚数：{{ $event->pictures->count() }}／キー：{{ $event->auth_key }}
-                <a href="{{ route('events.update') }}">編集する</a><br>
-            </p>
+        <div class="col-md-6 offset-md-3 text-center">
+            <div class="card">
+            <h3 class="card-title">{{ $event->title }}</h3>
+            <p class="card-text">掲載期間：{{ $event->release_date }} - {{ $event->end_date }} 枚数：{{ $event->pictures->count() }}／キー：{{ $event->auth_key }}</p>
+                <a href="{{ route('events.update') }}" ><button type="button" class="btn btn-primary">編集する</button></a>
+            <div class="row">
             @foreach($event->pictures as $picture)
-                <div class="picture">
-                    <img class="picture-thumbnail" src="{{ \Storage::url($picture->path) }}"><br>
-                    Updated<br>{{ $picture->updated_at->format('Y/m/d H:i') }}
+                <div class="col-md-4">
+                    <img class="img img-thumbnail" src="{{ Storage::url($picture->path) }}"><br>
+                    <!--Updated<br>{{ $picture->updated_at->format('Y/m/d H:i') }}-->
                 </div>
             @endforeach
+            </div>
+            </div>
         </div>
     @endforeach
+    </div>
+</div>
 @endsection
