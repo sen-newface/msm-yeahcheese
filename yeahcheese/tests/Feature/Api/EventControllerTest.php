f473aefc082ca7619c687a62b8bdad5fbff1bb97
kei-s16 <kenta.sasaki@sencorp.co.jp>
2020-05-21 15:51:40 +0900

    各項目のテスト内容を記述

--- yeahcheese/tests/Feature/Api/EventControllerTest.php
+++ yeahcheese/tests/Feature/Api/EventControllerTest.php
@@ -121,26 +121,103 @@ class EventControllerTest extends TestCase
 
     public function testDenyUpdateRequestWithoutId()
     {
-        
+        $request = [
+            'title' => 'update event title',
+            'release_date' => '2020-06-01',
+            'end_date' => '2020-06-30',
+        ];
+
+        $response = $this->put('api/events/update', $request);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'messages' => [
+                'IDが見つかりませんでした'
+            ]
+        ]);
     }
 
     public function testDenyInvalidUpdateRequest()
     {
-    
+        $event = factory(Event::class)->create();
+
+        $request = [
+            'id' => $event->id,
+            'title' => '96y82AhDHF4ghuzFUsVbeFxEsiQTPGikPSGWwMeiJhDxaGBD2YsfCRN3txBJPGKf986WLGAAj5arsN6eTX8KWtnFaM96i5FUiV5nYrcWpVtiynMGnPa9bm684UJ4YnSmcJ4iWZxEAZUkz8m38fEGyxYZHkLc7butrB4EaxTrPRKcbCTwYSfTVjZ2NuAQmuRwWaas9xT5hBMQ3rr3FG2JBWjhDPKKyiEu68eW5EuscF6ynBB6HxuKGub9LgDiMmeNhue4JjXgXwZ5Ca9yzj8NXep2mxG9PGmj59arQRWnjJbW',
+            'release_date' => strtotime('+7 day' . $event->end_date),
+            'end_date' => $event->end_date,
+        ];
+
+        $response = $this->put('api/events/update', $request);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'messages' => [
+                'イベントタイトルは255文字まで設定できます',
+                'イベント公開開始日は公開終了日より前の日付である必要があります',
+            ]
+        ]);
     }
 
     public function testDenyInvalidTitle()
     {
-    
+        $event = factory(Event::class)->create();
+
+        $request = [
+            'id' => $event->id,
+            'title' => '96y82AhDHF4ghuzFUsVbeFxEsiQTPGikPSGWwMeiJhDxaGBD2YsfCRN3txBJPGKf986WLGAAj5arsN6eTX8KWtnFaM96i5FUiV5nYrcWpVtiynMGnPa9bm684UJ4YnSmcJ4iWZxEAZUkz8m38fEGyxYZHkLc7butrB4EaxTrPRKcbCTwYSfTVjZ2NuAQmuRwWaas9xT5hBMQ3rr3FG2JBWjhDPKKyiEu68eW5EuscF6ynBB6HxuKGub9LgDiMmeNhue4JjXgXwZ5Ca9yzj8NXep2mxG9PGmj59arQRWnjJbW',
+        ];
+
+        $response = $this->put('api/events/update', $request);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'messages' => [
+                'イベントタイトルは255文字まで設定できます',
+            ]
+        ]);
     }
 
     public function testDenyInvalidReleaseDate()
     {
-    
+        $event = factory(Event::class)->create();
+
+        $request = [
+            'id' => $event->id,
+            'release_date' => strtotime('+7 day' . $event->end_date),
+        ];
+
+        $response = $this->put('api/events/update', $request);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'messages' => [
+                'イベント公開開始日は公開終了日より前の日付である必要があります',
+            ]
+        ]);
     }
 
     public function testDenyInvalidEndDate()
     {
-    
+        $event = factory(Event::class)->create();
+
+        $request = [
+            'id' => $event->id,
+            'end_date' => strtotime('-7 day' . $event->release_date),
+        ];
+
+        $response = $this->put('api/events/update', $request);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'messages' => [
+                'イベント公開終了日はイベント公開開始日より後の日付である必要があります',
+            ]
+        ]);
     }
 }
