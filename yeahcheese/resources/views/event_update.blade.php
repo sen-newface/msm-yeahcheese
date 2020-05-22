6172cc236fadb07dfefa0144a473d4685134020e
minami <m.kaori25@gmail.com>
2020-05-22 16:40:47 +0900

    エラーメッセージ用の場所を用意

--- yeahcheese/resources/views/event_update.blade.php
+++ yeahcheese/resources/views/event_update.blade.php
@@ -8,11 +8,18 @@
     <div id="update">
         <p>イベントタイトル</p>
         <input v-model="title">
+        <p>@{{ title_error_msg }}</p>
+
         <p>公開開始日</p>
         <input type="date" v-model="release_date">
+        <p>@{{ error_release_date_msg }}</p>
+
         <p>公開終了日</p>
         <input type="date" v-model="end_date">
+        <p>@{{ error_end_date_msg }}</p>
+
         <button type="submit" @click="updateEvent">更新</button>
+
         <p>@{{ message }}</p>
     </div>
 
