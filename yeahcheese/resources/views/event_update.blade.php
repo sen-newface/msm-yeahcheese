4530db5cd849ac60afba493a348dc56a9de875fd
minami <m.kaori25@gmail.com>
2020-05-22 12:24:03 +0900

    期間のフォームがカレンダーになるように変更

--- yeahcheese/resources/views/event_update.blade.php
+++ yeahcheese/resources/views/event_update.blade.php
@@ -9,9 +9,9 @@
         <p>イベントタイトル</p>
         <input v-model="title">
         <p>公開開始日</p>
-        <input v-model="release_date">
+        <input type="date" v-model="release_date">
         <p>公開終了日</p>
-        <input v-model="end_date">
+        <input type="date" v-model="end_date">
         <button type="submit" @click="updateEvent">更新</button>
     </div>
 
