e895a274cb9b3c51db18f65c8a56ac725c6cdb68
minami <m.kaori25@gmail.com>
2020-05-22 16:42:21 +0900

    update時のバリデーションを追加

--- yeahcheese/resources/js/index.js
+++ yeahcheese/resources/js/index.js
@@ -11,6 +11,47 @@ new Vue(
             release_date : '',
             end_date : '',
             message : '',
+            title_error_msg : '',
+            error_release_date_msg : '',
+            error_end_date_msg : '',
+            error_flag : false,
+        },
+        watch:{
+            title: function (newTitle) {
+                if (newTitle.length > 10) {
+                    this.title_error_msg = "10文字以下にしてください"
+                    this.error_flag = false
+                } else {
+                    this.title_error_msg = ""
+                    this.error_flag = true
+                }
+            },
+            release_date: function (newDate) {
+                /* 開始日を今日以降にするかどうか?
+                const today = new Date()
+                const YYYY = today.getFullYear()
+                const MMint = today.getMonth()+1;
+                const MMstr = ('0' + MMint).slice(-2)
+                const DD = ('0' + today.getDate()).slice(-2)
+                const todaystr = YYYY + '-' + MMstr + '-' + DD
+                */
+                if ( Date.parse(newDate) > Date.parse(this.end_date) ) {
+                    this.error_release_date_msg = "公開開始日は公開終了日以前にしてください"
+                    this.error_flag = false
+                } else {
+                    this.error_release_date_msg = ""
+                    this.error_flag = true
+                }
+            },
+            end_date: function (newDate) {
+                if ( Date.parse(newDate) < Date.parse(this.release_date) ) {
+                    this.error_end_date_msg = "公開終了日は公開開始日以降にしてください"
+                    this.error_flag = false
+                } else {
+                    this.error_end_date_msg = ""
+                    this.error_flag = true
+                }
+            }
         },
         created: function () {
             api.fetchEvent(this.event_id).then(
@@ -31,11 +72,16 @@ new Vue(
                     release_date : this.release_date,
                     end_date : this.end_date
                 }
-                api.updateEvent(request).then(
-                    response => {
-                        this.message = "イベント情報が更新されました";
-                    },
-                )
+                if (this.error_flag) {
+                    api.updateEvent(request).then(
+                        response => {
+                            this.message = "イベント情報が更新されました";
+                            console.log(new Date());
+                        },
+                    )
+                } else {
+                    this.message = "更新できませんでした"
+                }
             }
         }
     }
