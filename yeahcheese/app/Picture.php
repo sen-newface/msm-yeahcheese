945b95b17d2904b94f0e7339059937238a99c4a0
nonhina <hinano.miyajima@sencorp.co.jp>
2020-05-25 15:21:59 +0900

    フォームリクエストを作成したので削除

--- yeahcheese/app/Picture.php
+++ yeahcheese/app/Picture.php
@@ -11,15 +11,6 @@ class Picture extends Model
         return $this->belongsTo('App\Event');
     }
 
-    public static $storeRules = [
-        'file' => ['required', 'file', 'mimes:jpeg,jpg', 'between:100,1024'],
-    ];
-
-    public static $messages = [
-        'file.mimes' => 'jpgまたはjpeg形式にしてください',
-        'file.between' => '100KB以上、1MB以下の画像にしてください',
-    ];
-
     protected $fillable = [
         'path',
         'event_id'
