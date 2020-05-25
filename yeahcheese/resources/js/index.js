cfdb40706e80b6a3a7641a9171726fb6942ee61c
kei-s16 <kenta.sasaki@sencorp.co.jp>
2020-05-25 10:44:38 +0900

    画像削除に失敗した際にエラーメッセージを表示するように変更

--- yeahcheese/resources/js/index.js
+++ yeahcheese/resources/js/index.js
@@ -33,11 +33,10 @@ Vue.component('picture-item',
     computed: {
       path: function () {
         return 'https://yeahcheese.localapp.jp/storage/app/' + this.receivedPath;
-      }
+      },
     },
     template: '\
               <div>\
-                  <p>{{ this.id }}</p>\
                   <img v-bind:src="this.path" width="100%" class="img">\
                   <a  v-on:click="$emit(\'remove-picture\', id)"\
                       class="btn btn-primary">削除する</a>\
@@ -53,6 +52,7 @@ new Vue(
       event_id: JSON.parse(document.currentScript.dataset.eventId),
       pictures: [],
       uploadImage: null,
+      removeError: false,
     },
     created: function () {
       api.getPicturesList(this.event_id).then(
@@ -65,25 +65,30 @@ new Vue(
     },
     methods: {
       removePicture (id) {
+        let index = this.pictures.findIndex((p) => p.id === id);
         api.removePicture(id).then(
           pictureRemoveResponse => {
-            let index = this.pictures.findIndex((p) => p.id === id);
             this.pictures.splice(index, 1);
+            this.removeError = false;
           },
-          // TODO: API利用に失敗した際の処理を記述する
-          errors => console.error(errors),
+          errors => {
+            this.removeError = true;
+            console.error(errors);
+          },
         );
       },
     },
     template: '\
         <div>\
           <input v-on:change="" type="file">\
+          <p v-if="this.removeError">削除に失敗しました。時間を置いてやりなおしてください。</p>\
           <div class="container-fluid">\
               <div class="row">\
                   <div class="col-4 my-2" v-for="p in pictures">\
                       <picture-item\
                           :id = "p.id"\
                           :received-path = "p.path"\
+                          :remove-error = false\
                           @remove-picture="removePicture($event)"\
                       ></picture-item>\
                   </div>\
