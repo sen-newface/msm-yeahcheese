61c823db3bc0fee78a9f6ff8cfdb861073303e5e
kei-s16 <kenta.sasaki@sencorp.co.jp>
2020-05-25 09:01:35 +0900

    不要なコメント行を削除

--- yeahcheese/database/factories/EventFactory.php
+++ yeahcheese/database/factories/EventFactory.php
@@ -6,11 +6,9 @@ use App\Event;
 use Faker\Generator as Faker;
 
 $factory->define(Event::class, function (Faker $faker) {
-    //$datetime = $faker->dateTimeThisYear;
 
     return [
         'title' => $faker->word,
-        // 'release_date' => $datetime->format('Y-m-d H:i'),
         'release_date' => $faker->dateTimeBetween('now', '+1 month')->format('Y-m-d H:i'),
         'end_date' => $faker->dateTimeBetween($faker->dateTimeBetween('now', '+7 days'), '+1 years')->format('Y-m-d H:i'),
         'auth_key' => hash('fnv132', $faker->name),
