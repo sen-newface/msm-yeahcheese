de56f380e539d4694bb03504d075fd05c158d43b
kei-s16 <kenta.sasaki@sencorp.co.jp>
2020-05-26 17:30:41 +0900

    index.jsからイベント編集コンポーネントを分離

---
+++ yeahcheese/resources/js/components/EventEditorComponent.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <p>イベントタイトル</p>
+    <input v-model="title">
+    <p>{{ error_title_msg }}</p>
+
+    <p>公開開始日</p>
+    <input type="date" v-model="release_date">
+    <p>{{ error_release_date_msg }}</p>
+
+    <p>公開終了日</p>
+    <input type="date" v-model="end_date">
+    <p>{{ error_end_date_msg }}</p>
+
+    <button type="submit" @click="updateEvent">更新</button>
+
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import api from '../api';
+import Vue from 'vue';
+
+export default {
+  name: 'EventEditorComponent',
+  props: {
+    eventId: Number,
+  },
+  data: {
+    title : '',
+    release_date : '',
+    end_date : '',
+    message : '',
+    error_title_msg : '',
+    error_release_date_msg : '',
+    error_end_date_msg : '',
+    error_flag : false,
+  },
+  created: function () {
+    api.fetchEvent(this.eventId).then(
+      eventResponse => {
+        const data = eventResponse.data.data;
+        this.title = data.title;
+        this.release_date = data.release_date;
+        this.end_date = data.end_date;
+      },
+      // TODO: API利用に失敗した際の処理を記述する リクエストの再送信など
+      errors => console.error(errors)
+    );
+  },
+  watch: {
+    title: function (newTitle) {
+      if (newTitle.length > 255 || newTitle.length < 1) {
+        this.error_title_msg = "255文字以下のタイトルを入力してください"
+        this.error_flag = false
+      } else {
+        this.error_title_msg = ""
+        this.error_flag = true
+      }
+    },
+    release_date: function (newDate) {
+      /* 開始日を今日以降にするかどうか?
+      const today = new Date()
+      const YYYY = today.getFullYear()
+      const MMint = today.getMonth()+1;
+      const MMstr = ('0' + MMint).slice(-2)
+      const DD = ('0' + today.getDate()).slice(-2)
+      const todaystr = YYYY + '-' + MMstr + '-' + DD
+      */
+      if ( Date.parse(newDate) > Date.parse(this.end_date) ) {
+        this.error_release_date_msg = "公開開始日は公開終了日以前にしてください"
+        this.error_flag = false
+      } else {
+        this.error_release_date_msg = ''
+        this.error_end_date_msg = ''
+        this.error_flag = true
+      }
+    },
+    end_date: function (newDate) {
+      if ( Date.parse(newDate) < Date.parse(this.release_date) ) {
+        this.error_end_date_msg = "公開終了日は公開開始日以降にしてください"
+        this.error_flag = false
+      } else {
+        this.error_end_date_msg = ''
+        this.error_release_date_msg = ''
+        this.error_flag = true
+      }
+    },
+  },
+  methods: {
+    updateEvent: function() { // 更新ボタンを押したときに呼ばれる
+      const request = {
+        id : this.eventId,
+        title : this.title,
+        release_date : this.release_date,
+        end_date : this.end_date
+      }
+      if (this.error_flag) {
+        api.updateEvent(request).then(
+          response => {
+            this.message = "イベント情報が更新されました";
+            console.log(new Date());
+          },
+        )
+      } else {
+        this.message = "更新できませんでした"
+      }
+    }
+  },
+}
+</script>
