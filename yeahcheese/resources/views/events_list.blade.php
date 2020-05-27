f7d2c2625d3fbdcedd1ab811a2b7689d96e0ab82
minami <m.kaori25@gmail.com>
2020-05-27 11:18:41 +0900

    画面サイズによる表示を変更

--- yeahcheese/resources/views/events_list.blade.php
+++ yeahcheese/resources/views/events_list.blade.php
@@ -12,7 +12,7 @@
     <!-- ユーザーが作成した全イベント情報を表示 --> 
     @foreach($events as $event)
     <div class="row mb-4">
-        <div class="col-md-12">
+        <div class="col-12">
             <div class="card shadow-sm">
                 <a class="text-body" href="{{ route('events.show', ['auth_key' => $event->auth_key]) }}"><h3 class="card-header">{{ $event->title }}</h3></a>
                 <div class="card-body row">
@@ -25,10 +25,12 @@
                         <a class="btn btn-outline-primary btn-lg" href="{{ route('events.edit', ['event' => $event]) }}" role="button">編集する</a>
                     </div>
                     <div class="col-md-8">
-                        <div class="row">
+                        <div class="row m-2">
                             @foreach($event->pictures->take(3) as $picture)
-                                <div class="col-md-4">
-                                    <img class="img img-thumbnail" width="80%" src="{{ Storage::url($picture->path) }}">
+                                <div class="col-4 d-flex align-items-center justify-content-center">
+                                    <div class="border rounded">
+                                        <img class="list_thumbnail m-2" src="{{ Storage::url($picture->path) }}">
+                                    </div>
                                 </div>
                             @endforeach
                         </div>
