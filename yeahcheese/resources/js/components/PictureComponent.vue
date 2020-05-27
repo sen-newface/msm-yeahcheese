4d76c1603fae471fbc15ee05c3411545ad305189
kei-s16 <kenta.sasaki@sencorp.co.jp>
2020-05-27 16:19:58 +0900

    画像を管理する配列にundefinedが入らないように #189 暫定

--- yeahcheese/resources/js/components/PictureComponent.vue
+++ yeahcheese/resources/js/components/PictureComponent.vue
@@ -107,6 +107,10 @@ export default {
       api.postPicture(data).then(
         picturePostResponse => {
           this.pictures.push(picturePostResponse.data.data);
+          // 追加された要素がundefinedならその場で削除する
+          if (this.pictures[this.pictures.length - 1] == null) {
+            this.pictures.splice(-1)[0];
+          }
         },
         errors => {
           this.storeError = true;
