82ecb737c799f17351297c85a826922843990c40
minami <m.kaori25@gmail.com>
2020-05-27 10:26:17 +0900

    エラーメッセージ表示位置とインデント修正

--- yeahcheese/resources/views/event_show.blade.php
+++ yeahcheese/resources/views/event_show.blade.php
@@ -11,33 +11,33 @@
     </div>
 
     <div class="container">
-    <div class="row">
-        <!-- 写真一覧表示 -->
-        @if ($errors->any())
-            <p>{{ $errors->first() }}</p>
-        @endif
-        @foreach($pictures as $picture)
-        <div class="col-sm-4 my-2 d-flex align-items-center justify-content-center">
-            <img src="{{ \Storage::url($picture->path) }}" class="show_item" data-toggle="modal" data-target="#picture{{ $picture->id }}">
-        </div>
+        <div class="row">
+            <!-- 写真一覧表示 -->
+            @if ($errors->any())
+                <p class="col">{{ $errors->first() }}</p>
+            @endif
+            
+            @foreach($pictures as $picture)
+            <div class="col-sm-4 my-2 d-flex align-items-center justify-content-center">
+                <img src="{{ \Storage::url($picture->path) }}" class="show_item" data-toggle="modal" data-target="#picture{{ $picture->id }}">
+            </div>
 
-        <div class="modal fade" id="picture{{ $picture->id }}">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <img src="{{ \Storage::url($picture->path) }}" class="aligncenter size-full w-100">
+            <div class="modal fade" id="picture{{ $picture->id }}">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <img src="{{ \Storage::url($picture->path) }}" class="aligncenter size-full w-100">
+                        </div>
                     </div>
                 </div>
             </div>
+            @endforeach
         </div>
-        
-        @endforeach
-    </div>
     </div>
 </div>
 @endsection
