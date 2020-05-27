1451fc675942e267dfa5feb9aa68759fffaae7e1
nonhina <hinano.miyajima@sencorp.co.jp>
2020-05-27 16:00:39 +0900

    フォームリクエストを作成したので削除

--- yeahcheese/app/Event.php
+++ yeahcheese/app/Event.php
@@ -56,25 +56,6 @@ class Event extends Model
         return $query->where('release_date', '<=', $date);
     }
 
-    public static $storeRules = [
-        'title' => ['required', 'max:255'],
-        'release_date' => ['required', 'date', 'after_or_equal:today'],
-        'end_date' => ['required', 'date', 'after:release_date'],
-    ];
-
-    public static $messages = [
-        'title.required' => 'イベントタイトルは必須項目です',
-        'title.max' => 'イベントタイトルは255文字まで設定できます',
-
-        'release_date.required' => 'イベント公開開始日は必須項目です',
-        'release_date.date' => 'イベント公開開始日は日付形式で入力してください',
-        'release_date.after_or_equal' => 'イベント公開開始日は本日以降の日付である必要があります',
-
-        'end_date.required' => 'イベント公開終了日は必須項目です',
-        'end_date.date' => 'イベント公開終了日は日付形式で入力してください',
-        'end_date.after' => 'イベント公開終了日はイベント公開開始日より後の日付である必要があります',
-    ];
-
     protected $fillable = [
         'title',
         'release_date',
